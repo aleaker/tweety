669cc5e7b4c0df116ebcb46c16549b34148e424b
aleaker <alejoakerfeld@gmail.com>
2019-09-25 10:03:00 -0300

    repo inicializado

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use(function(req,res,next){
- console.log("soy un middleware")
- next();
-})
+app.use("/special", function(req, res, next) {
+  res.send("soy un middleware");
+  next();
+});
 
-app.get("/",function(req,res,next){
-    res.send("hola")
-})
+app.get("/", function(req, res, next) {
+  res.send("hola");
+});
 
+app.get("/special", function(req, res, next) {
+  res.send("soy especial");
+});
+
+app.listen(3000, function() {
+  console.log("soy el server y estoy escuchando!");
+});
